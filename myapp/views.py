14ef5a9b187089256b942fa032bc1f99bb3805ed
Hema Dalavayi <gayathri.dhs@gmail.com>
2024-04-19 00:31:04 -0500

    Initial Commit

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Product
+from django.shortcuts import get_object_or_404
+
+def product_index(request):
+    products = Product.objects.all()
+    return render(request,'products/index.html', {'products':products})
+
+def product_show(request,id):
+    product = get_object_or_404(Product,id=id)
+    return render(request,'products/show.html', {'product':product})
